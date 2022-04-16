a500da9b21b89c5e5a96fe17abde775bf829a6a2
Luiz Fernando Barros Moragas <luiz.moragas@hotmail.com>
2022-04-16 01:36:14 +0100

    creating end points

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {});
+app.post("/", (req, res) => {});
+app.put("/", (req, res) => {});
+app.delete("/", (req, res) => {});
+
 app.listen(8080, () => {
   `Running at port 8080!`;
 });
