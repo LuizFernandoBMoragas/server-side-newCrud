c21dd87ae1b56e34f3dd33cea6290bb1f362d405
Luiz Fernando Barros Moragas <luiz.moragas@hotmail.com>
2022-04-17 21:02:25 +0100

    received json from front and sent to database

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 
 const FoodModel = require("./models/Food");
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(
   "mongodb+srv://luizfbmoragas:P4d48iv9!@crud.rmaj7.mongodb.net/food?retryWrites=true&w=majority",
   { useNewUrlParser: true }
 );
 
-app.get("/", async (req, res) => {
+app.post("/insert", async (req, res) => {
+  const foodName = req.body.foodName;
+  const days = req.body.days;
+  const foodType = req.body.foodType;
+  const foodOrigin = req.body.foodOrigin;
+  const foodDrink = req.body.foodDrink;
+
   const food = new FoodModel({
-    foodName: "Paella",
-    expirationDate: 3,
-    typeOfFood: "Candy",
-    countryOfOrigin: "Spain",
-    bestDrinkAsASideDish: "Wine",
+    foodName: foodName,
+    expirationDate: days,
+    typeOfFood: foodType,
+    countryOfOrigin: foodOrigin,
+    bestDrinkAsASideDish: foodDrink,
   });
 
   try {
